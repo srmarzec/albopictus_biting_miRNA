f2919b7d475f787044688a6d8163845a3e7dfd59
srmarzec <srmarzec@users.noreply.github.com>
2022-08-17 11:19:18 -0400

    Create contaminants_index.sh

---
+++ Upstream/scripts/contaminants_index.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=contaminants_index --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=sm3679@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=4G
+
+#-----------------------------------------------------------------------------#
+# This script runs makes a genome index with bowtie #
+#-----------------------------------------------------------------------------#
+
+module load bowtie2/2.4.4
+
+#- Set variables ----------------------------------------------------------------#
+
+ref_genome=/home/sm3679/albopictus_biting_miRNA/albopictus_genome/albo_contaminants.fasta
+index_path_prefix=/home/sm3679/albopictus_biting_miRNA/albopictus_genome/contaminant_index/
+
+
+#- RUN bowtie ----------------------------------------------------------------#
+
+bowtie-build ${ref_genome} ${index_path_prefix}
+
+
+#- Unload module----------------#
+module unload bowtie2/2.4.4
+
+#- FIN -----------------------------------------------------------------------#
