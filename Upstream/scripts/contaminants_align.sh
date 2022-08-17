ef5f473d08dc26c59d9df1dd9067a39de78a0d61
srmarzec <srmarzec@users.noreply.github.com>
2022-08-17 13:48:08 -0400

    Create contaminants_align.sh

---
+++ Upstream/scripts/contaminants_align.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=contaminants_align --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=sm3679@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=4G
+
+#-----------------------------------------------------------------------------#
+# This runs bowtie2 for contaminant mapping#
+#---------------------------------------------------------------------#
+ 
+#- Load Module--------------------------------------------#
+module load bowtie2/2.4.4
+ 
+#- DEFINE FILE LOCATIONS--------------------------------------------#
+ 
+index_path=/home/sm3679/albopictus_biting_miRNA/albopictus_genome/contaminant_index/contaminant
+input_dir=/home/sm3679/albopictus_biting_miRNA/trim_dir
+filtered_dir=/home/sm3679/albopictus_biting_miRNA/filtered_dir
+sam_dir=/home/sm3679/albopictus_biting_miRNA/sam_dir
+ 
+#- RUN command ----------------#
+files=(${input_dir}/*.cln.fastq.gz)
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} .cln.fastq.gz`
+
+bowtie2 -x ${index_path} -U ${input_dir}/${base}.cln.fastq.gz -S ${sam_dir}/${base}.flt.cln.sam --un-gz ${filtered_dir}/${base}.flt.fastq.gz
+
+done 
+
+#- Unload module----------------#
+module unload bowtie2/2.4.4
+
+#- FIN -----------------------#
